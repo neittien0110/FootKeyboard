6cb3eaa6f1277a81412b7fb32bbdf2720fe5740d
Nguyen Duc Tien <neittien0110@gmail.com>
2024-08-04 07:42:29 +0700

    Squashed commit of the following:
    commit e7f8951e58798e1544cc198bca5be7529692b560
    Author: Nguyen Duc Tien <neittien0110@gmail.com>
    Date:   Sun Aug 4 07:16:31 2024 +0700
    
        Đã cho phép đọc cú pháp USER_FORMAT
    
    commit b9677515f225ee40a5607073d074780ab103f127
    Author: Nguyen Duc Tien <neittien0110@gmail.com>
    Date:   Wed Jul 31 11:25:39 2024 +0700
    
        Gộp xử lý nút bấm vào 1 vòng lặp (phase)
    
    commit 7110c51e848756f96a12d19a0c7278947fd583f8
    Author: Nguyen Duc Tien <neittien0110@gmail.com>
    Date:   Wed Jul 31 10:02:11 2024 +0700
    
        Nâng cấp thư viện BleKeyboard để cho phép định nghĩa chuỗi kí tự cần gửi phức tạp hơn với hàm SendKeys theo mã ASCII_FORMAT
    
    commit 56e9ded629a8140b6684f8385cf0a4d08e598114
    Author: Nguyen Duc Tien <neittien0110@gmail.com>
    Date:   Mon Jul 29 20:32:01 2024 +0700
    
        Thay thế delay bị block bằng kiểm tra thời gian non-blocking

---
+++ sources/firmware/src/BleKeyboardBuilder.cpp
@@ -0,0 +1,202 @@
+#include <Arduino.h>
+#include "BleKeyboardBuilder.h"
+#include <cstdint>
+
+BleKeyboardBuilder::BleKeyboardBuilder(std::string deviceName , std::string deviceManufacturer , uint8_t batteryLevel ) :
+    BleKeyboard(deviceName , deviceManufacturer , batteryLevel )
+{
+};
+
+/**
+ * @brief Gửi kí tự ra giao tiếp HID 
+ * @param ASCII_FORMAT cmd Chuôi kí tự cần gửi theo cú pháp ASCII_FORMAT. Bắt buộc zero-ending.
+ * @example     const char myCmd[] = {KEY_F4, 0};  SendKeys(myCmd);   
+ *              const char myCmd[] = {KEY_LEFT_SHIFT, 'c', 'H', 'a', ASCII_RELEASE_CODE, KEY_LEFT_SHIFT, 'o',' ', 'T', 0}; SendKeys(myCmd); 
+  */
+void BleKeyboardBuilder::SendKeys(const ASCII_FORMAT * cmd) {
+    static char ch;
+    static uint8_t i;
+
+    i=0;
+    while (true){
+        ch = cmd[i];
+        if (ch == 0) { // Nếu là kí tự kết thúc chuỗi thì nhả tất cả các phím và dừng lại
+            releaseAll();    
+            return;
+        } else if (ch < 0x80) { // Nếu là kí tự thường thì hiện thị
+            press(ch);              
+            //bleSendKeys.release(ch);
+            //Không cần release, vì press kí tự sau sẽ trở thành release kí tự trước (ngoại trừ kí tự diều khiển)
+            // Tuy nhiên, hết sức lưu ý phản ứng của thư viện trong 2 trường hợp. ví dụ với cùng doạn mã sau
+            //     const char myCmd[] = {ASCII_CODE_PRESS_LEFT_SHIFT, 'c', 'H', 'a', 'o', ASCII_CODE_RELEASE_LEFT_SHIFT, 0};
+            //      SendKeys(myCmd);    
+            // - CÓ    bleSendKeys.release(ch);   THÌ KẾT QUẢ TRẢ VỀ LÀ    CHao
+            // - KHÔNG bleSendKeys.release(ch);   THÌ KẾT QUẢ TRẢ VỀ LÀ    CHAO
+        } else { 
+            // Nếu là kí tự đặc biệt chính là vai trò của mã ASCII_FORMAT đây
+            if (ch != ASCII_RELEASE_CODE) {
+                press(ch);
+            } else {
+                i++;
+                ch = cmd[i];
+                release(ch);
+            }
+        }
+        i++; // Chuyển sang kí tự tiếp theo
+    };
+}
+
+
+typedef struct {
+    uint16_t checksum;
+    uint8_t code;
+} CheckSUM2CODE;
+
+/** Bảng qui đổi từ checksum của USER_FORMAT ==> KeyCode */
+CheckSUM2CODE checksum2codes[] = { 
+    {631, KEY_LEFT_CTRL},          // {~CTRL}
+    {498, KEY_LEFT_ALT},          // {~ALT}
+    {793, KEY_LEFT_SHIFT},          // {~SHIFT}
+    {784, KEY_RIGHT_CTRL},          // {~RCTRL}
+    {649, KEY_RIGHT_ALT},          // {~RALT}
+    {947, KEY_RIGHT_SHIFT},          // {~RSHIFT}
+    {477, KEY_LEFT_CTRL},          // {CTRL}
+    {344, KEY_LEFT_ALT},          // {ALT}
+    {637, KEY_LEFT_SHIFT},          // {SHIFT}
+    {628, KEY_RIGHT_CTRL},          // {RCTRL}
+    {492, KEY_RIGHT_ALT},          // {RALT}
+    {791, KEY_RIGHT_SHIFT},          // {RSHIFT}
+    {1102, KEY_BACKSPACE},          // {BACKSPACE}
+    {581, },          // {BREAK}
+    {1031, KEY_CAPS_LOCK},   // {CAPSLOCK}
+    {732, KEY_DELETE},       // {DELETE}
+    {490, KEY_DOWN_ARROW},   // {DOWN}
+    {299, KEY_END},          // {END}
+    {634, KEY_RETURN},       // {ENTER}
+    {301, KEY_ESC},          // {ESC}
+    {440, KEY_HOME},         // {HOME}
+    {821, KEY_INSERT},       // {INSERT}
+    {485, KEY_LEFT_ARROW},   // {LEFT}
+    {883, 0xDB},             // {NUMLOCK}
+    {459, KEY_PAGE_DOWN},    // {PGDN}
+    {493, KEY_PAGE_UP},      // {PGUP}
+    {589, KEY_PRTSC},        // {PRTSC}
+    {640, KEY_RIGHT_ARROW},  // {RIGHT}
+    {1323, 0xCF},            // {SCROLLLOCK}
+    {284, KEY_TAB},          // {TAB}
+    {202, KEY_UP_ARROW},     // {UP}
+    {133, KEY_F1},           // {F1}
+    {135, KEY_F2},           // {F2}
+    {137, KEY_F3},           // {F3}
+    {139, KEY_F4},           // {F4}
+    {141, KEY_F5},           // {F5}
+    {143, KEY_F6},           // {F6}
+    {145, KEY_F7},           // {F7}
+    {147, KEY_F8},           // {F8}
+    {149, KEY_F9},           // {F9}
+    {210, KEY_F10},          // {F10}
+    {213, KEY_F11},          // {F11}
+    {216, KEY_F12},          // {F12}
+    {219, KEY_F13},          // {F13}
+    {222, KEY_F14},          // {F14}
+    {225, KEY_F15},          // {F15}
+    {228, KEY_F16},          // {F16}
+    {199, KEY_BACKSPACE},          // {BS}
+    {314, KEY_DELETE},          // {DEL}
+    {345, KEY_INSERT},          // {INS}
+    {123, '{'},          // {{}
+    {125, '}'},          // {}}
+};
+
+
+
+/**
+ * @brief Chuyển đổi định dạng dành cho USER thành định dạng RAW
+ * 
+ * @param USER_FORMAT *    request . Xem cú pháp https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys
+ * @param ASCII_FORMAT *   command
+ * @return int      Độ dài chuỗi kết quả command 
+ */
+int BleKeyboardBuilder::ConvertFormat(const USER_FORMAT * request, ASCII_FORMAT *  command){
+    uint8_t i, oi,j;
+    int16_t start_brace_i, checksum;
+    char ch;
+   
+    i = 0;
+    oi = 0;
+    start_brace_i = -1; // <0 tức là chưa tìm thấy kí tự {
+
+    while (true) {
+        // Lấy ra kí tự
+        ch = request[i];  
+        if (ch == 0) {
+            command[oi] = 0;    // Đánh dấu điểm cuối cùng
+            break;
+        } else if (ch == '{') {
+            // Đã tìm thấy điểm bắt đầu ngoặc
+            start_brace_i = i;
+            // Tìm điểm cuối của ngoặc
+            i = i+1;
+            // Tính checksum là tổng mã ascii của tất cả các kí tự x vị trí, áp dụng dể tìm chuỗi cho nhanh. bản chất là đánh indexing chuỗi
+            checksum = 0; 
+            while (request[i] != '}' and (request[i] != 0)) { 
+                checksum = (checksum >> 1) + request[i] * (i - start_brace_i);               
+                // Serial.print(request[i]);
+                // Serial.print("*(");
+                // Serial.print(i);
+                // Serial.print("-");
+                // Serial.print(start_brace_i);
+                // Serial.print(") ==> checksum  = ");
+                // Serial.println(checksum); 
+                i++;
+            }
+            // Nếu không tìm thấy, tức là lỗi cú pháp rồi, loại.
+            if (request[i] == 0) {
+                oi = -start_brace_i;        // Lỗi ở vị trí dấu ngoặc
+                break;
+            }
+
+            // Lúc này command[start_brace_i:i] chứa chuỗi như {ENTER},  {PGUP}
+            // Xác định mã của các phím đặc biệt và gán vào command.      
+            for (j=0;j < 52; j++) {
+                if (checksum2codes[j].checksum == checksum) {
+                    // Đã tìm thấy mã của điều khiển
+                    if (j < 0x6) {      // là dạng release của 3 phím Ctrl, Alt, Shift
+                        command[oi] = ASCII_RELEASE_CODE;
+                        oi++;
+                    }
+                    command[oi] = checksum2codes[j].code ;   
+                    oi++;
+                    break;
+                }
+            }
+            // Đánh dấu đã xử lý xong ngoặc
+            start_brace_i = -1; // Shift, Ctrl, Alt
+        }  else {
+            command[oi] = request[i];
+            oi++;
+        }
+        i++; // CHuyển sang kí tự tiếp theo
+    }
+    
+    return oi;
+}
+
+/* Tóm tắt:
+      - việc press() một kí tự thường sẽ làm release() kí tự trước đó.
+            bleKeyboardBuilder.press('a');                              bleKeyboardBuilder.press('a');
+            bleKeyboardBuilder.release('a');    ====tương đương==>      bleKeyboardBuilder.press('b');
+            bleKeyboardBuilder.press('b');
+      - việc press() hoặc release() một kí tự thường sẽ KHÔNG release() kí tự điều khiển trước đó. 
+        Đoạn code sau sẽ làm phím a được gõ vô hạn và phím shift giữ vô hạn.
+            bleKeyboardBuilder.press('a');              
+            bleKeyboardBuilder.press(KEY_RIGHT_SHIFT);  
+      - việc press() hoặc release() một kí tự điểu khiển sẽ KHÔNG release() kí tự thường trươc đó
+            bleKeyboardBuilder.press(KEY_RIGHT_SHIFT);  
+            bleKeyboardBuilder.press('a');   
+      - việc press kí tự chữ hoa sẽ được hiểu là kèm theo cả press và release phím SHIFT
+                                                                bleKeyboardBuilder.press(KEY_RIGHT_SHIFT);
+            bleKeyboardBuilder.press('A');     ====tương đương==>      bleKeyboardBuilder.press('a');
+                                                                bleKeyboardBuilder.release('a');        
+                                                                bleKeyboardBuilder.release(KEY_RIGHT_SHIFT);    
+*/
--- sources/firmware/src/main.cpp
+++ sources/firmware/src/main.cpp
@@ -1,99 +1,221 @@
 /**
- * Bộ bàn phím dùng chân điều khiển, phù hợp với các hoạt động nhập số liệu hoặc chơi game. 
+ * Bộ bàn phím dùng chân điều khiển, phù hợp với các hoạt động nhập số liệu hoặc chơi game.
  * Thiết bị giao tiếp máy tính dưới dạng bàn phím bluetooth, không cần driver điều khiển.
+ * Sử dụng: 
+ *   - như bàn phím thường với các nút bấm to đạp bằng chân
+ *   - có thêm 2 socket dạng XH2.54 làm đầu chờ cho dạng nút vặn như volumn.
+ *   - Khi mới khởi động, tiếp điện, bấm và giữ nút BOOT để đưa mạch về trạng thái kiểm tra xuất xưởng Self_Test
+ * Cấu hình
+ *   - Sử dụng máy tính gửi lệnh serial ở tốc độ 115200, 8bit, 0 stop bit.
+ *   - Cú pháp lệnh:  <button>=<userformat>
+ *     Lưu ý rằng cú pháp này cho giao tiếp máy-máy, đòi hỏi chuỗi phải chính xác.
+ *     <userformat> theo cú pháp <https://github.com/neittien0110/FootKeyboard/tree/master/sources/firmware>
+ *   - Ví dụ lệnh: PEDAL1=Chao
+ *   - Ví dụ lệnh: PEDAL2={ENTER}
+ *   - Ví dụ lệnh: PEDAL3={CTRL}{F4}{~CTRL}
+ *   - Ví dụ lệnh: PEDAL3={ATL}{SHIFT}EC{~ATL}{~SHIFT}
+ * Thuật toán mã kĩ tự
+ *           USER_FORMAT  -------------------------> ASCII_FORMAT -----------------------------------> KEYCODE
+ *     (clean, clear for user)     (simple, fast for cpu, encryp press|release keycode)               (standard)
  */
 #include <Arduino.h>
-#include <BleKeyboard.h>
+#include "BleKeyboardBuilder.h"
 
-//#define DEBUG
 
-#define LED_BUILTIN 8
-#define BUTTON_BOOT 9
+//#define DEBUG_SERIAL_SYNTAX      // Debug lỗi cú pháp của bộ phân tích USER_FORMAT
 
-#define MAX_BUTTONS 4       // Số lượng pedal/nút bấm tối đa
+#define LED_BUILTIN 8   //Led mặc định sẵn có trên ESP32-Super Mini
+#define BUTTON_BOOT 9   //Nút bấm mặc định sẵn có trên ESP32-Super Mini
 
-#define PIN_PEDAL01 1       // Kết nối tới pedal số 1
-#define PIN_PEDAL02 2       // Kết nối tới pedal số 2
-#define PIN_PEDAL03 10      // Kết nối tới pedal số 3
-#define PIN_PEDAL04 7       // Kết nối tới pedal số 4
-#define PIN_VAR1 0          // Kết nối tới biến trở / cảm biến lực FSR
-#define PIN_VAR2 3          // Kết nối tới biến trở / cảm biến lực FSR
+#define MAX_BUTTONS 4 // Số lượng pedal/nút bấm tối đa
+
+#define PIN_PEDAL01 1  // Kết nối tới pedal số 1
+#define PIN_PEDAL02 2  // Kết nối tới pedal số 2
+#define PIN_PEDAL03 10 // Kết nối tới pedal số 3
+#define PIN_PEDAL04 7  // Kết nối tới pedal số 4
+#define PIN_VAR1 0     // Kết nối tới biến trở / cảm biến lực FSR
+#define PIN_VAR2 3     // Kết nối tới biến trở / cảm biến lực FSR
 
 /**
- * @brief Trạng thái tích cực theo mức. Ví dụ 0, 1. 
+ * @brief Trạng thái tích cực theo mức. Ví dụ 0, 1.
  * @example Ví dụ  digitalRead(3) == PEDAL_ACTIVE_LOGIC
  */
 #define PEDAL_ACTIVE_LOGIC 0
 #define PEDAL_DEACTIVE_LOGIC 1
 
+/**
+ * @brief 4 trạng thái của phím bấm, tạo thành chu trình 4 bước gồm sườn, mức bấm. sườn, mức nhả.
+ */
 enum KEYSTATUS
 {
-    KEYFREE = 0,
-    KEYDOWN = 1,
-    KEYPRESS = 2,
-    KEYUP = 3,
+    KEYFREE = 0,    // phím không được bấm
+    KEYDOWN = 1,    // phím vừa được bấm
+    KEYPRESS = 2,   // phím bấm giữ
+    KEYUP = 3,      // phím vừa được nhả 
 };
 
+
 const uint button_pins[MAX_BUTTONS] = {PIN_PEDAL01, PIN_PEDAL02, PIN_PEDAL03, PIN_PEDAL04};
-uint button_prevalues[MAX_BUTTONS];  /// Giá trị trước đó, ở dạng digital 0/1 của nút bấm
-uint button_curvalues[MAX_BUTTONS];  /// Giá trị hiện thời, ở dạng digital 0/1 của nút bấm
-KEYSTATUS button_status[MAX_BUTTONS];/// Trạng thái hiện thời của các nút bấm
+uint button_prevalues[MAX_BUTTONS];   /// Giá trị trước đó, ở dạng digital 0/1 của nút bấm
+uint button_curvalues[MAX_BUTTONS];   /// Giá trị hiện thời, ở dạng digital 0/1 của nút bấm
+KEYSTATUS button_status[MAX_BUTTONS]; /// Trạng thái hiện thời của các nút bấm
 
 /// @brief Thời điểm của lần lặp trước đó
 unsigned long TimeOfPreLoop;
 
-int i;
-
 /**
  * @brief  Handler điều khiển giao tiếp HID Keyboard BLE
  */
-BleKeyboard bleKeyboard("Foot keyboard", "Bluetooth Device Manufacturer", 100);
+BleKeyboardBuilder bleKeyboardBuilder("Foot Keyboard", "Bluetooth Device Manufacturer", 100);
 
+#pragma region SERIAL_CONFIG
+    /// Kích thước bộ đệm đọc lệnh từ Serial
+    #define SERIAL_BUFFER_SIZE 10
+    // Lệnh điều khiển nhận được từ Serial, có dạng userformat
+    USER_FORMAT SerialCommand[SERIAL_BUFFER_SIZE];
+    /// Phân tách mỗi dòng lệnh điều khiển thành 2 vùng. key=value.
+    /// Key ở điểm bắt đầu của commandline, nên luôn trùng với điểm đầu của lệnh
+    char * cmdkey = SerialCommand;
+    const char KEY01[] = "01";   // mã cấu hình tương ứng với chân PIN_PEDAL01
+    const char KEY02[] = "02";   // mã cấu hình tương ứng với chân PIN_PEDAL02
+    const char KEY03[] = "03";   // mã cấu hình tương ứng với chân PIN_PEDAL03
+    const char KEY04[] = "04";   // mã cấu hình tương ứng với chân PIN_PEDAL04
+
+    // Phân tách mỗi dòng lệnh điều khiển thành 2 vùng. key=value
+    USER_FORMAT * cmdvalue = NULL;    
+    /// Chuỗi các kí tự cần gửi, ứng với mỗi pedal. Cú pháp đơn kí tự, là bộ mã ASCII nhưng tận dụng mã ascii điều khiển để thành mã phím bấm điều khiển
+    ASCII_FORMAT button_sendkeys[MAX_BUTTONS][SERIAL_BUFFER_SIZE];  
+
+/**
+ * @brief Cấu hình chức năng các phím/pedal qua Serial
+ *
+ */
+void SerialConfiguration()
+{
+    uint8_t i;
+    /// Nếu không có kết nối serial thì kết thúc luôn
+    if (Serial.available() <= 0)
+    {
+        return;
+    }
+    
+    /// Đọc lệnh từ Serial
+    int rlen = Serial.readBytesUntil('\n',SerialCommand, SERIAL_BUFFER_SIZE);
+    SerialCommand[rlen] = 0; // đánh dấu kết thúc chuỗi, nếu không sẽ dính với phần còn lại của lệnh trước.
+
+#pragma region PARSING_COMMAND
+    cmdvalue = NULL;
+    /// Phân tích parsing
+    for (i = 0; i < rlen; i++) {// Đọc từng kí tự
+        if (SerialCommand[i] == '=') {
+            SerialCommand[i]=0; // Đánh dấu Zero kết thúc chuỗi
+            cmdvalue = & SerialCommand[i+1];
+            break;
+        }
+    }
+
+    // Nếu độ dài của key, value dưới 2 kí tự là không hợp lệ. Quá ngắn
+    if ((i < 2) || ((rlen - i) < 2)) {
+        return;
+    };
+#pragma endregion PARSING_COMMAND
+
+#pragma region ASSIGN_COMMAND
+    /// Gán lệnh cho các nút bấm/pedal tương ứng
+    if (strcasecmp(cmdkey, KEY01) == 0) {
+        i = 0;
+    } else if (strcasecmp(cmdkey, KEY02) == 0) {
+        i = 1;
+    } else if (strcasecmp(cmdkey, KEY03) == 0) {
+        i = 2;
+    } else if (strcasecmp(cmdkey, KEY04) == 0) {
+        i = 3;
+    } else {
+        // mã phím bấm không phù hợp, kết thúc luôn
+        return;
+    }
+    strcpy(button_sendkeys[i], cmdvalue);
+
+    // Debug
+    Serial.print("Button="); Serial.print(i);
+    Serial.print(", UserFormat="); Serial.print(button_sendkeys[i]);
+
+#pragma endregion ASSIGN_COMMAND
+}
+
+#pragma endregion SERIAL_CONFIG
 
 
 /**
- * @brief Trạng thái hoạt động của 4 pedal
+ * @brief Chế độ tự kiểm tra các chức năng hoạt động, dành cho DEV và QA
+ * 
  */
-KEYSTATUS pedal01_status = KEYFREE;
-KEYSTATUS pedal02_status = KEYFREE;
-KEYSTATUS pedal03_status = KEYFREE;
-KEYSTATUS pedal04_status = KEYFREE;
+void Self_Test(){
+    // Gửi các kí tự về 
+    const char myCmd[] = {KEY_LEFT_SHIFT, 'c', 'H', 'a', ASCII_RELEASE_CODE, KEY_LEFT_SHIFT, 'o',' ', 'T', 0};
+    bleKeyboardBuilder.SendKeys(myCmd);    
+
+    BleKeyboardBuilder::ConvertFormat("{PGDN}", button_sendkeys[0]);
+    Serial.print(button_sendkeys[0]);
+}
 
 /** Kết nối với thiết bị */
-bool TryToConnect(){
+bool TryToConnect()
+{
     uint8_t led_blink = HIGH;
 
     // Nếu đã kết nối rồi thì hủy kết nối
-    if (bleKeyboard.isConnected()) {
-        bleKeyboard.end();
+    if (bleKeyboardBuilder.isConnected())
+    {
+        bleKeyboardBuilder.end();
     }
 
-    //Bắt đầu phiên
-    bleKeyboard.begin();
+    // Bắt đầu phiên
+    bleKeyboardBuilder.begin();
     // Led nháy báo hiệu đang tìm thiết bị kết nôi
-    while (!bleKeyboard.isConnected())
+    while (!bleKeyboardBuilder.isConnected())
     {
-        led_blink = ! led_blink;
+        led_blink = !led_blink;
         digitalWrite(LED_BUILTIN, led_blink);
-        delay(100);        
+        delay(100);
     }
     return true;
 }
 
-
 void setup()
-{
+{   
+    uint8_t i;
     // Cấu hình cho chân pin led mặc định
     pinMode(LED_BUILTIN, OUTPUT);
 
     // Thiết lập kếnh cấu hình phím và debug
     Serial.begin(115200);
 
-    // Kết nối thiết bị liên tục cho tới khi thành công.    
-    TryToConnect(); 
-#ifdef DEBUG    
-    Serial.println("BLE: connected");
-#endif    
+    // Cấu hình cho nút bấm chế độ
+    pinMode(BUTTON_BOOT, INPUT);    
+
+#if defined(DEBUG_SERIAL_SYNTAX)
+    //Ö  C  h  a  o     T  i  e   n    {  BS  } +  -   * /  TAB
+    //d6 43 68 61 6f 20 54 69 65 6e 20 7b b2 7d 2b 2d 2a 2f b3 0a
+    // . PD 
+
+    const char * myRequest = "{PGDN}Chao Tien {{}{BACKSPACE}{}}+-*/{TAB}";
+    char myCommand[100];
+    int res;
+    while (true) {    
+        res = BleKeyboardBuilder::ConvertFormat(myRequest, myCommand);
+
+        // Đèn báo hiệu sẵn sàng
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(500);
+        digitalWrite(LED_BUILTIN, HIGH);        
+        Serial.println(myCommand);        
+        delay(500);
+    }
+#else
+
+    // Kết nối thiết bị liên tục cho tới khi thành công.
+    TryToConnect();
 
     // Led sáng,  báo hiệu kết nối bluetooth thành công
     digitalWrite(LED_BUILTIN, LOW);
@@ -102,26 +224,49 @@ void setup()
     // Khởi tạo trạng thái cho các chân pedal có chức năng đóng/cắt:
     // - nối vào các chân pin dạng INPUT với điện trở kéo lên bên trong
     // - trạng thái ban đầu là nhả phím
-    for (i = 0 ; i < MAX_BUTTONS; i++) {
+    for (i = 0; i < MAX_BUTTONS; i++)
+    {
         pinMode(button_pins[i], INPUT_PULLUP);
         button_status[i] = KEYFREE;
     }
-    
+
     // 2 nối vào các chân pin dạng INPUT với điện trở kéo xuống ở ngoài board
     pinMode(PIN_VAR1, INPUT);
     pinMode(PIN_VAR2, INPUT);
 
-    // Đánh dấu thời điểm bắt đầu chạy vòng lặp. 
+    // Thiết lập chức năng mặc định cho 4 pedal
+    BleKeyboardBuilder::ConvertFormat("{PGDN}", button_sendkeys[0]);
+    BleKeyboardBuilder::ConvertFormat("{PGUP}", button_sendkeys[1]);
+    BleKeyboardBuilder::ConvertFormat("{ENTER}", button_sendkeys[2]);
+    BleKeyboardBuilder::ConvertFormat("{CTRL}{F4}{~CTRL}", button_sendkeys[3]);
+
+    // Đánh dấu thời điểm bắt đầu chạy vòng lặp.
     TimeOfPreLoop = millis();
+
+    if ((digitalRead(BUTTON_BOOT) == 0 ) && (digitalRead(PIN_PEDAL01) == PEDAL_ACTIVE_LOGIC)) {
+        Self_Test();  //blocking
+        // Treo thiết bị với đèn báo 2 lần chớp
+        while (true){
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(80);
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(100);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(80);            
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(700);    
+        }
+    }
 
     // Đèn báo hiệu sẵn sàng
     digitalWrite(LED_BUILTIN, LOW);
     sleep(1);
     digitalWrite(LED_BUILTIN, HIGH);
-#ifdef DEBUG       
-    Serial.println("in loop key press..");
+
+
 #endif    
 }
+
 
 /** Xác định trạng thái phim bấm trong chu trình DOWN, PRESS, UP, FREE */
 KEYSTATUS DetectKeyWordflow(uint8_t pre, uint8_t cur)
@@ -150,74 +295,62 @@ KEYSTATUS DetectKeyWordflow(uint8_t pre, uint8_t cur)
 
 void loop()
 {
+    static int8_t i;
     static uint8_t tmp;
-    static uint8_t isDown;      // = true nếu có 1 nút bấm/pedal nào đó được bấm
+    static uint8_t isDown; // = true nếu có 1 nút bấm/pedal nào đó được bấm
 
     // Lấy thời điểm hiện tại
     unsigned long currentMillis = millis();
 
-    if ( (isDown && (currentMillis - TimeOfPreLoop < 200))   // Nếu có phím bấm thì phải 200ms mới chạy lại
-        || (!isDown && (currentMillis - TimeOfPreLoop < PERIOD_OF_BUTTON_SCAN_LOOP))) // Nếu không bấm phím thì cứ 50ms lại kiểm tra lại
-    {    
+    if ((isDown && (currentMillis - TimeOfPreLoop < 180))     // Nếu có phím bấm thì phải 180ms mới chạy lại.
+        || (!isDown && (currentMillis - TimeOfPreLoop < 15))) // Nếu không bấm phím thì cứ 15ms lại kiểm tra lại
+    {
         return;
     }
-    TimeOfPreLoop = currentMillis;// Đã vượt qua none-blocking delay
 
-    // đọc trạng thái của cả 4 nút
-    for (i = 0 ; i < MAX_BUTTONS; i++) {
+    //--------------PHASE 1: đọc trạng thái các nút bấm/pedal
+    for (i = 0; i < MAX_BUTTONS; i++)
+    {
         // Lấy giá trị phím bấm mới
         tmp = digitalRead(button_pins[i]);
         // Nếu 3 lần đọc liên tiếp có dạng 010 hoặc 101 thì chuyển về 000 hoặc 111 tương ứng
-        if ((tmp == button_prevalues[i]) && (tmp != button_curvalues[i]) ) {
+        if ((tmp == button_prevalues[i]) && (tmp != button_curvalues[i]))
+        {
             button_curvalues[i] = tmp;
         };
-        //Lưu lại lịch sử, chóng nhảy phím
+        // Lưu lại lịch sử, chóng nhảy phím
         button_prevalues[i] = button_curvalues[i];
         button_curvalues[i] = tmp;
 
         // Xác định trạng thái phím
         button_status[i] = DetectKeyWordflow(button_prevalues[i], button_curvalues[i]);
+    }
 
-#ifdef DEBUG
-        Serial.print(button_status[i]);
-        Serial.print(",");
-#endif        
-    }    
-
-#ifdef DEBUG    
-    Serial.println("");
-#endif    
-
+    //--------------PHASE 2: hành động
     isDown = false;
-    if (button_status[0] == KEYDOWN)
-    {
-        bleKeyboard.write(KEY_PAGE_DOWN); 
-        isDown = true;
-    }
-    if (button_status[1] == KEYDOWN)
-    {
-        bleKeyboard.write(KEY_PAGE_UP); 
-        isDown = true;        
-    }
-    if (button_status[2] == KEYDOWN)
-    {
-        bleKeyboard.write(KEY_RETURN);  
-        isDown = true;        
-    }
-    if (button_status[3] == KEYDOWN)
-    {
-        bleKeyboard.press(KEY_LEFT_CTRL);
-        bleKeyboard.press(KEY_F4);
-        delay(10);
-        bleKeyboard.releaseAll();
-        isDown = true;        
+    // đọc trạng thái của cả 4 nút
+    for (i = 0; i < MAX_BUTTONS; i++)
+    {    
+        if (button_status[i] == KEYDOWN)
+        {
+            
+            bleKeyboardBuilder.SendKeys(button_sendkeys[i]);       //bleKeyboardBuilder.write(KEY_PAGE_DOWN);
+            isDown = true;
+        }
     }
 
-    // Bật tắt đèn tùy theo bấm phím
-    if (isDown) {
+    //--------------PHASE 3: hậu xử lý nếu cần
+    // Nếu có phím được bấm thì mới cần delay để tránh xử lý 2 phím liên tiếp, còn không thì bỏ qua
+    // Có cần thiết không nhỉ?
+    if (isDown)
+    {
         digitalWrite(LED_BUILTIN, LOW);
-    }        
-    else {
-        digitalWrite(LED_BUILTIN, HIGH);    
     }
+    else
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+
+    // Cấu hình chức năng các phím/pedal qua Serial
+    SerialConfiguration();
 }
